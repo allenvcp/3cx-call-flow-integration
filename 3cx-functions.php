be8a7f05e7e4094ed28936c779c5e817d59eb3f4
autrydev <stephen@vetcompandpen.com>
2017-08-29 20:57:53 +0000

    Consolidated functions into one file. Deleted old function script files.

--- 3cx-functions.php
+++ 3cx-functions.php
@@ -1,13 +1,73 @@
 <?php
 
 	require_once __DIR__ . '/vendor/autoload.php';
-	include __DIR__ . '/get-contact.php';
+	require_once __DIR__ . '/ISToken.php';
+
+	function getContact($infusionsoft, $phoneNum)
+    {
+    	
+    	//Note to future Allen or Stephen
+		//Custom fields are prepended with an underscore
+		//E.g. '_CustomField'
+    	
+        $table = 'Contact';
+        $fieldName = 'Phone1';
+        $returnFields = array(
+            
+            'Email',
+            'FirstName',
+            'Id',
+            'LastName',
+            'OwnerID',
+            'Phone1'
+        );
+    	
+        $results = $infusionsoft->data('xml')->findByField($table, 1, 0 ,$fieldName, $phoneNum, $returnFields);
+
+        $results = json_encode($results);
+        
+        return $results;	
+        
+    }
+    
+    function addTag($infusionsoft, $contactId, $tagId){
+
+    	$results = $infusionsoft->contacts('xml')->addToGroup($contactId, $tagId);
+
+	}
+	
+	function addNote($infusionsoft, $contactId, $ownerId){
+
+		//Add Note in to Contact Record
+		//Update CreationNotes and ActionDescription for final build
 	
 	
+		$infusionsoft->data('xml')->add('ContactAction', array(    	
+	        'ContactId' 	    => $contactId, 
+	        'CreationNotes'     => $body,  		       // Note Description
+	        'CreationDate'      => server_time,
+	        'ActionDate'        => server_time,
+	        'CompletionDate'    => server_time,
+	        'OpportunityId'     => 0,
+	        'ActionDescription' => 'New Call From: ',  // Note Title
+	        'ActionType'        => 'UPDATE',		   // Match the Type in Infusionsoft
+	        'UserID'            => $setOwnerId,	       // User ID
+	        'IsAppointment'     => 1,
+	        'Priority'          => 3,
+	        'LastUpdatedBy'     => 1,			       // User ID
+	        'LastUpdated'       => server_time,
+    	    'CreatedBy'         => $setOwnerId,        // User ID
+    	    'Accepted'          => 1
+    	));
+
+	}
+	
+	$infusionsoft = getISToken();
+
 	$phoneNum = '(904) 796-0559';
 	//$phoneNum = $_POST['phone'];
 	
-	$result = json_decode(getContact($phoneNum),true);
+	$result = json_decode(getContact($phoneNum,$infusionsoft),true);
 
 	echo $result[0]['Id'];
 ?>
